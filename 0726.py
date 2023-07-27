12593f999498923026116819a7ba2dbf1bfb78d9
dakyung <140672633+ledakyung@users.noreply.github.com>
2023-07-27 10:42:56 +0900

    Add files via upload

---
+++ 0726.py
@@ -0,0 +1,12 @@
+#a=(input(첫 번째 수를 입력하세요'))
+#b=(input('두 번째 수를 입력하세요'))
+
+a,b=int (int(' 두 개의 정수를 입력하세요:').split()
+#10 20 a=10 b=20
+#printint(a)+printint(b)
+#print(c)
+
+a,b=map(int,input('두 개의 정수를 입력하세요:').split(,))
+
+c=a+b
+print(c)         
